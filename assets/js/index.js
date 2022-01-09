0759aa35eccf5a0796e92e483dfe24fad853afdf
DescrySama <pierrehugot35@gmail.com>
2022-01-09 10:55:23 +0100

    ???

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,6 @@
-// let navbarNavSelect = document.getElementById('navbarFull');
-// let SectionSelect = document.getElementById('home')
-
-// if (windows.scrollY > (navbarNavSelect.offsetTop + navbarNavSelect.offsetHeight)) {
-//     navbarNavSelect.classList.add('bg-dark');
-// }
-
-
 window.addEventListener("scroll", function() {
     var elementTarget = document.getElementById("scrolladder");
-    if (window.scrollY > (elementTarget.offsetTop + elementTarget.offsetHeight - 1)) {
+    if (window.scrollY > (elementTarget.offsetTop + elementTarget.offsetHeight - 10)) {
         let navbarFull = document.getElementById('navbarFull')
         navbarFull.classList.add('bg-dark');
     } else {
